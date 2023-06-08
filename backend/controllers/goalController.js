0fd3965d23d233aa8b573c5bdbad72dec3d54374
Dipin Bhattarai <bhattaraidipin@gmail.com>
2023-06-08 16:10:05 +0545

    initialized contoroller and routes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc    get goals
+// @route   GET /api/goals
+// @access  private
+const getGoals = (req, res) => {
+  res.json({ messege: `Get goals` });
+};
+
+// @desc    get goals
+// @route   POST /api/goals
+// @access  private
+const setGoal = (req, res) => {
+  res.json({ messege: `Set goals` });
+};
+
+// @desc    update goal
+// @route   GET /api/goals/:id
+// @access  private
+const updateGoal = (req, res) => {
+  res.json({ messege: `update goals ${req.params.id}` });
+};
+
+// @desc    delete goal
+// @route   DELETE /api/goals/:id
+// @access  private
+const deleteGoal = (req, res) => {
+  res.json({ messege: `delete goals ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const express = require("express");
+const port = process.env.PORT || 5000;
+const dotevv = require("dotenv").config();
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => console.log(`port created in ${port}`));
