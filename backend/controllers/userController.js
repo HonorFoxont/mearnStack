e8bbea69919d891f23218bd9e349992bb6ad95bc
Dipin Bhattarai <bhattaraidipin@gmail.com>
2023-06-12 10:52:47 +0545

    minor documentation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,11 +8,13 @@ const asyncHandler = require("express-async-handler");
 // @route   POST /api/user
 // @access  public
 const registerUser = asyncHandler(async (req, res) => {
+  //Checking input data
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please enter all feilds");
   }
+  //Checking if user already exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
@@ -21,7 +23,7 @@ const registerUser = asyncHandler(async (req, res) => {
   //Hasing Password
   const salt = await bcrypt.genSalt(10);
   const hassedPassword = await bcrypt.hash(password, salt);
-
+  //Adding user in database
   const user = await User.create({
     name,
     email,
@@ -44,11 +46,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/user/login
 // @access  public
 const loginUser = asyncHandler(async (req, res) => {
+  //Checking input data
   const { email, password } = req.body;
   if (!email || !password) {
     res.status(400);
     throw new Error("Please enter all feilds");
   }
+  //Getting user and checking password
   const user = await User.findOne({ email });
   if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
